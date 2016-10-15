1605e48748fd15c2d337519c602c310d838e04ff
Mateus Guimarães <mateus.jatene@gmail.com>
2016-10-15 05:35:34 -0300

    basic stuff

--- tests/SimpleCartTest.php
+++ tests/SimpleCartTest.php
@@ -2,15 +2,48 @@
 
 namespace Mateusjatenee\SimpleCart\Tests;
 
+use Mateusjatenee\SimpleCart\Cart;
+use Mateusjatenee\SimpleCart\Providers\SimpleCartServiceProvider;
 use Mateusjatenee\SimpleCart\SimpleCart;
-use PHPUnit_Framework_TestCase as PHPUnit;
+use Orchestra\Testbench\TestCase;
 
-class SimpleCartTest extends PHPUnit
+class SimpleCartTest extends TestCase
 {
-    public function testExample()
-    {
-        $simplecart = new SimpleCart();
 
-        $this->assertTrue(true);
+    /**
+     * Set the package service provider.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [SimpleCartServiceProvider::class];
+    }
+
+    /** @test */
+    public function it_adds_items_to_a_cart()
+    {
+        $cart = $this->getCart();
+
+        $cart->add(123, 'Regular T-Shirt', 1, 20);
+
+        $this->assertEquals([
+            [
+                'id' => 123,
+                'name' => 'Regular T-Shirt',
+                'quantity' => 1,
+                'price' => 20,
+            ],
+        ], $cart->content()->all());
+    }
+
+    private function getCart()
+    {
+        $session_manager = $this->app->make('session');
+        $dispatcher = $this->app->make('events');
+
+        return new Cart($session_manager, $dispatcher);
     }
 }
